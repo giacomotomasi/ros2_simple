39e995a2cdca7aa0083a85b301351bcca320b680
giacomotomasi <giacomotms@gmail.com>
2024-09-10 16:27:39 +0200

    Removed namespace.

--- src/MarkerSubscriber.cpp
+++ src/MarkerSubscriber.cpp
@@ -4,7 +4,7 @@ namespace ros2_simple {
     MarkerSubscriber::MarkerSubscriber() : Node("marker_subscriber_node") {
         RCLCPP_INFO(this->get_logger(), "Subscriber created.");
 
-        _marker_sub = this->create_subscription<visualization_msgs::msg::Marker>("/ros2_simple/marker_topic", 10, std::bind(&MarkerSubscriber::markerCallback, this, std::placeholders::_1));
+        _marker_sub = this->create_subscription<visualization_msgs::msg::Marker>("marker_topic", 10, std::bind(&MarkerSubscriber::markerCallback, this, std::placeholders::_1));
         // Initialize timer
         _timer_start = this->now();
     }
